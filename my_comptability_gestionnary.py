4d7182c22ae7f3fbe804eeac4944df22f846b00e
sKillseries <skillseries.infosec@gmail.com>
2023-06-09 20:55:45 +0200

    modif code et excel (refonte)

--- my_comptability_gestionnary.py
+++ my_comptability_gestionnary.py
@@ -4,8 +4,24 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 from termcolor import colored
 
+def depensevariable():
+    global depensesvariables
+    retrait = input("Combien d'argent avez-vous retiré au guichet ce mois-ci ?: ")
+    coiffeur = input("Combien a coûté le coiffeur ?: ")
+    vetement = input("combien avez-vous dépensé en vêtements ce mois-ci ?: ")
+    alimentation = input("A combien s'élève le montant de vos courses ce mois-ci ?: ")
+    cosmetique = input("Combien avez-vous dépensé en cosmétique ?: ")
+    loisir = input("Combien as-tu dépensé en loisir ?: ")
+    autre = input("Montant autre dépense ?: ")
+    depvar = float(retrait) + float(coiffeur) + float(vetement) + float(alimentation) + float(cosmetique) + float(loisir) + float(autre)
+    depensesvariables = round(depvar, 2)
+    print(colored(f"Vos dépenses variables s'élèvent à {depensesvariables}€ ce mois-ci. \n", 'yellow'))
+    menu()
+
 def depensefixe():
     global depensesfixes
+    epargne = input("Combien avez-vous épargné ce mois-ci ?: ")
+    investissement = input("Combien avez-vous investi ce mois-ci ?: ")
     telephone = input("Combien coûte votre abonnement téléphonique ce mois-ci ?: ")
     internet = input("Combien coûte votre abonnement internet ce mois-ci ?: ")
     loyer = input("Combien coûte votre loyer ce mois-ci ?: ")
@@ -13,24 +29,10 @@ def depensefixe():
     auto = input("Combien coûte votre traite auto ce mois-ci ?: ")
     electricite = input("Quelle est la somme de votre facture d'électricité ce mois-ci ?: ")
     eau = input("Quelle est la somme de votre facture d'eau ce mois-ci ?:")
-    depenses = float(telephone) + float(internet) + float(loyer) + float(transport) + float(auto) + float(electricite) + float(eau)
-    depensesfixes = round(depenses, 2)
+    depfixe = float(epargne) + float(investissement) + float(telephone) + float(internet) + float(loyer) + float(transport) + float(auto) + float(electricite) + float(eau)
+    depensesfixes = round(depfixe, 2)
     print(colored(f"Vos dépenses fixes s'élèvent à {depensesfixes}€ ce mois-ci. \n", 'yellow'))
-    menu()
-
-def investissement():
-    global investissements
-    investissement = input("Combien avez-vous investi ce mois-ci ?: ")
-    investissements = float(investissement)
-    print(colored(f"Vous avez investi {investissements}€ ce mois-ci. \n", 'green'))
-    depensefixe()
-
-def epargne():
-    global epargnes
-    epargne = input("Combien avez-vous épargné ce mois-ci ?: ")
-    epargnes = float(epargne)
-    print(colored(f"Vous avez épargné {epargnes}€ ce mois-ci. \n", 'green'))
-    investissement()
+    depensevariable()
 
 def revenu():
     global revenues
@@ -43,9 +45,18 @@ def revenu():
     revenus = float(salaire) + float(locations) + float(dividendes) + float(interets) + float(redevances)
     revenues = round(revenus, 2)
     print(colored(f"Vos revenus ce mois-ci s'élève à {revenues}€. \n", 'green'))
-    epargne()
+    depensefixe()
 
 def saveandproceed():
+    reste = float(revenues) - float(depensesfixes) - float(depensesvariables)
+    restes = round(reste, 2)
+    print(f"""
+    Bilan:
+    Revenus: {revenues}€
+    Dépenses fixes: {depensesfixes}€
+    Dépenses variables: {depensesvariables}€
+    Restes:  {restes}€
+    """)
     print(colored("La saisie a été enregistré dans le fichier my_comptability_sheet.xlsx.", 'green'))
     answer = input("Voulez-vous saisir un autre mois (o/N) ?: ")
     if answer == "O":
@@ -88,86 +99,74 @@ def menu():
     choix = input("Veuillez sélectionnez le numéro du mois: ")
     if choix == "1":
         sheet.cell(row=5, column=3).value = revenues
-        sheet.cell(row=5, column=4).value = epargnes
-        sheet.cell(row=5, column=5).value = investissements
-        sheet.cell(row=5, column=6).value = depensesfixes
+        sheet.cell(row=5, column=4).value = depensesfixes
+        sheet.cell(row=7, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "2":
         sheet.cell(row=6, column=3).value = revenues
-        sheet.cell(row=6, column=4).value = epargnes
-        sheet.cell(row=6, column=5).value = investissements
-        sheet.cell(row=6, column=6).value = depensesfixes
+        sheet.cell(row=6, column=4).value = depensesfixes
+        sheet.cell(row=7, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "3":
         sheet.cell(row=7, column=3).value = revenues
-        sheet.cell(row=7, column=4).value = epargnes
-        sheet.cell(row=7, column=5).value = investissements
-        sheet.cell(row=7, column=6).value = depensesfixes
+        sheet.cell(row=7, column=4).value = depensesfixes
+        sheet.cell(row=7, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "4":
         sheet.cell(row=8, column=3).value = revenues
-        sheet.cell(row=8, column=4).value = epargnes
-        sheet.cell(row=8, column=5).value = investissements
-        sheet.cell(row=8, column=6).value = depensesfixes
+        sheet.cell(row=8, column=4).value = depensesfixes
+        sheet.cell(row=8, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "5":
         sheet.cell(row=9, column=3).value = revenues
-        sheet.cell(row=9, column=4).value = epargnes
-        sheet.cell(row=9, column=5).value = investissements
-        sheet.cell(row=9, column=6).value = depensesfixes
+        sheet.cell(row=9, column=4).value = depensesfixes
+        sheet.cell(row=9, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "6":
         sheet.cell(row=10, column=3).value = revenues
-        sheet.cell(row=10, column=4).value = epargnes
-        sheet.cell(row=10, column=5).value = investissements
-        sheet.cell(row=10, column=6).value = depensesfixes
+        sheet.cell(row=10, column=4).value = depensesfixes
+        sheet.cell(row=10, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "7":
         sheet.cell(row=11, column=3).value = revenues
-        sheet.cell(row=11, column=4).value = epargnes
-        sheet.cell(row=11, column=5).value = investissements
-        sheet.cell(row=11, column=6).value = depensesfixes
+        sheet.cell(row=11, column=4).value = depensesfixes
+        sheet.cell(row=11, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "8":
         sheet.cell(row=12, column=3).value = revenues
-        sheet.cell(row=12, column=4).value = epargnes
-        sheet.cell(row=12, column=5).value = investissements
-        sheet.cell(row=12, column=6).value = depensesfixes
+        sheet.cell(row=12, column=4).value = depensesfixes
+        sheet.cell(row=12, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "9":
         sheet.cell(row=13, column=3).value = revenues
-        sheet.cell(row=13, column=4).value = epargnes
-        sheet.cell(row=13, column=5).value = investissements
-        sheet.cell(row=13, column=6).value = depensesfixes
+        sheet.cell(row=13, column=4).value = depensesfixes
+        sheet.cell(row=13, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "10":
         sheet.cell(row=14, column=3).value = revenues
-        sheet.cell(row=14, column=4).value = epargnes
-        sheet.cell(row=14, column=5).value = investissements
-        sheet.cell(row=14, column=6).value = depensesfixes
+        sheet.cell(row=14, column=4).value = depensesfixes
+        sheet.cell(row=14, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "11":
         sheet.cell(row=15, column=3).value = revenues
-        sheet.cell(row=15, column=4).value = epargnes
-        sheet.cell(row=15, column=5).value = investissements
-        sheet.cell(row=15, column=6).value = depensesfixes
+        sheet.cell(row=15, column=4).value = depensesfixes
+        sheet.cell(row=15, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "12":
         sheet.cell(row=16, column=3).value = revenues
-        sheet.cell(row=16, column=4).value = epargnes
-        sheet.cell(row=16, column=5).value = investissements
-        sheet.cell(row=16, column=6).value = depensesfixes
+        sheet.cell(row=16, column=4).value = depensesfixes
+        sheet.cell(row=16, column=5).value = depensesvariables
         wb.save("my_comptability_sheet.xlsx")
         saveandproceed()
     elif choix == "q":
